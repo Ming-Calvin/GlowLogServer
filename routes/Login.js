3d32f896dede1e58b7338d15ee6390139bc346be
小明 <2431165604@qq.com>
2024-08-31 09:00:50 +0800

    fix: add response format

---
+++ routes/Login.js
@@ -0,0 +1,17 @@
+
+
+// 用户注册接口: registerUser
+
+
+
+
+
+
+
+
+
+// 用户发送验证码接口: sendVerificationCode
+
+
+
+// 用户登录接口: loginUser
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
 const Router = require('koa-router');
+const {successResponse} = require("./responseData");
 const router = new Router()
-const usersRouter = require('./user');
-const whiteNoise = require('./whitenoise')
-const journal = require('./journal')
-const dialog = require('./dialog')
+// const usersRouter = require('./user');
+// const whiteNoise = require('./whitenoise')
+// const journal = require('./journal')
+// const dialog = require('./dialog')
 
 // example route
 router.get('/', async (ctx) => {
-  ctx.body = { message: 'Hello koa' }
+  ctx.body = successResponse({
+    message: 'Connect Successfully'
+  })
 })
 
-// Use users routes
-router.use(usersRouter.routes())
-
-// Use whiteNoise routes
-router.use(whiteNoise.routes())
-
-// Use journal routes
-router.use(journal.routes())
-
-// Use dialog routes
-router.use(dialog.routes())
+// // Use users routes
+// router.use(usersRouter.routes())
+//
+// // Use whiteNoise routes
+// router.use(whiteNoise.routes())
+//
+// // Use journal routes
+// router.use(journal.routes())
+//
+// // Use dialog routes
+// router.use(dialog.routes())
 
 
 
